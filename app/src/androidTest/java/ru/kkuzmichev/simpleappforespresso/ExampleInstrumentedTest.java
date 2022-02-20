34033eb9649d29bf1a19c04f3fecd4d86d91729a
Владислав Комсомоленко <space@space.local>
2022-02-20 22:17:52 +0300

    done

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,10 +1,21 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +28,36 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+    }
+
+    @Test
+    // positive Test
+    public void positiveCheckTextHome(){
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+
+        mainText.perform(click()).check(
+                matches(withText("This is home fragment")
+                ));
+    }
+    @Test
+    // negative Test
+    public void negativeCheckTextHome(){
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+
+        mainText.perform(click()).check(
+                matches(withText("This is gallery fragment")
+                ));
     }
 }
